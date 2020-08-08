374fc7960d142cc022a288ad8aba2a4fed21c413
fhl43211 <fhl43211@gmail.com>
2020-08-07 21:33:02 -0700

    Added more to symbol menu

--- StockBar/SymbolMenu.swift
+++ StockBar/SymbolMenu.swift
@@ -6,7 +6,7 @@
 
 import Cocoa
 func dailyPNLNumber(_ tradingInfo: TradingInfo, _ position: Position)->Double {
-    let unitSize = Double(position.unitSize) ?? 0
+    let unitSize = Double(position.unitSize) ?? .nan
     return (tradingInfo.currentPrice - tradingInfo.prevClosePrice)*unitSize
 }
 func dailyPNL(_ tradingInfo: TradingInfo, _ position: Position)->String {
@@ -14,11 +14,22 @@ func dailyPNL(_ tradingInfo: TradingInfo, _ position: Position)->String {
     return "DailyPnL: " + (tradingInfo.currency ?? "Price") + " " + pnlString
 }
 fileprivate func totalPNL(_ tradingInfo: TradingInfo, _ position: Position)->String {
-    let unitSize = Double(position.unitSize) ?? 0
-    let avgCost = Double(position.positionAvgCost) ?? 0
+    let unitSize = Double(position.unitSize) ?? .nan
+    let avgCost = Double(position.positionAvgCost) ?? .nan
     let pnl = (tradingInfo.currentPrice - avgCost)*unitSize
     let pnlString = String(format: "%+.2f", pnl)
     return "TotalPnL: " + (tradingInfo.currency ?? "Price") + " " + pnlString
+}
+fileprivate func totalPositionCost(_ position: Position)->String {
+    let unitSize = Double(position.unitSize) ?? .nan
+    let avgCost = Double(position.positionAvgCost) ?? .nan
+    return "Total position cost: "+String(format: "%.2f", unitSize*avgCost)
+}
+fileprivate func currentPositionValue(_ tradingInfo: TradingInfo, _ position: Position)->String {
+    let unitSize = Double(position.unitSize) ?? .nan
+    let positionValue = tradingInfo.currentPrice*unitSize
+    let positionString = String(format: "%.2f", positionValue)
+    return "Current position value: " + (tradingInfo.currency ?? "Price") + " " + positionString
 }
 
 final class SymbolMenu: NSMenu {
@@ -31,6 +42,10 @@ final class SymbolMenu: NSMenu {
         self.addItem(NSMenuItem.separator())
         self.addItem(withTitle: dailyPNL(tradingInfo, position), action: nil, keyEquivalent: "")
         self.addItem(withTitle: totalPNL(tradingInfo, position), action: nil, keyEquivalent: "")
+        self.addItem(withTitle: "Units: \(position.unitSize)", action: nil, keyEquivalent: "")
+        self.addItem(withTitle: "AvgPositionCost: \(position.positionAvgCost)", action: nil, keyEquivalent: "")
+        self.addItem(withTitle: totalPositionCost(position), action: nil, keyEquivalent: "")
+        self.addItem(withTitle: currentPositionValue(tradingInfo, position), action: nil, keyEquivalent: "")
     }
     
     required init(coder: NSCoder) {
